815253db77995aabfee24e014d576c8b8613dd89
nightstudio <31193051+nightstudio@users.noreply.github.com>
2017-08-21 15:08:22 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', 'XXX:XXX');
+define('BOT_TOKEN', '439729419:AAGPMkG3XmID2GVVz2aBueecmYyAvyWwZd0');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@ function processMessage($message) {
     // process incoming message
     $boolean = file_get_contents('booleans.txt');
     $booleans= explode("\n",$boolean);
-    $admin = "آیدی عددی شما";
+    $admin = "346578164";
     $message_id = $message['message_id'];
     $rpto = $message['reply_to_message']['forward_from']['id'];
     $chat_id = $message['chat']['id'];
